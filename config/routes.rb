5fbb789406d7b2aaa9307a10b8a7f3218fd6799b
kiyokawa <your.kohji12k@gmail.com>
2020-06-12 18:23:20 +0900

    static_pages作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'static_pages#top'
+  root to: 'static_pages#home'
+  resources :static_pages
+
+  # get 'static_pages/top'
+  # get 'static_pages/home'
 end
