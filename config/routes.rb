8dc47cbadf40302abab1f27466c54d428d16c432
kiyokawa <your.kohji12k@gmail.com>
2020-06-23 18:19:00 +0900

    CRUD機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'reviews#index'
+  #root to: 'static_pages#top' → sipnup & login
+  resources :reviews
 end
