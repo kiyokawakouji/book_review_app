edda73cf5b05e3fe0d7308987e6f12571b818f5c
kiyokawa <your.kohji12k@gmail.com>
2020-07-02 14:55:59 +0900

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'reviews#index'
   resources :reviews
+
+  # devise_for :users, controllers: {
+  #       sessions: 'users/sessions'
+  #     }
+  # end
 end
